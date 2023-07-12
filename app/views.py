ddda0ea8669b36eb9464e9eb809ac8334cdc6341
Anna Ilina <anna.ilina.1307@yandex.ru>
2023-07-12 13:47:57 +0300

    [UPD] converting code to MVC

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pandas as pd
 from pprint import pprint as pp
+import time
 
 from django.conf import settings
 from django.shortcuts import render
@@ -8,54 +9,73 @@ from django.http import JsonResponse, HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    #return render(request, 'index.html', {})
+    return render(request, 'indexapp.html', {})
 
 
 def get_filters(request):
     df = pd.read_csv(settings.CSV_DATA_PATH)
     filters = {
-        "hostnames": df['hostname'].fillna("undefined").unique().tolist(),
-        "models": df['cpu_model'].fillna("undefined").unique().tolist(),
-        "sites": df['site'].fillna("undefined").unique().tolist(),
-        "statuses": df['status'].fillna("undefined").unique().tolist(),
-        "owners": df['owner'].fillna("undefined").unique().tolist(),
+        #"hostname": df['hostname'].fillna("undefined").unique().tolist(),
+        #"model": df['cpu_model'].fillna("undefined").unique().tolist(),
+        "site": df['site'].fillna("undefined").unique().tolist(),
+        "status": df['status'].fillna("undefined").unique().tolist(),
+        "owner": df['owner'].fillna("undefined").unique().tolist(),
     }
     print(filters)
     return JsonResponse({'filters': filters})
 
-
+cash = None
 def get_data_by_filters(request):
+    global cash
+    if cash != None:
+        return cash
+    _start_time = time.time()
+    print("preliminary preparation")
     filters = request.GET.getlist('filters[]')
     # приходит с клиента локализованная дата
     start_datetime = datetime.strptime(request.GET.get('start'), '%Y-%m-%d %H:%M:%S')
     end_datetime = datetime.strptime(request.GET.get('end'), '%Y-%m-%d %H:%M:%S')
 
     # переводим её в ISO
-    start_datetime_iso = start_datetime.isoformat()
-    end_datetime_iso = end_datetime.isoformat()
-
+    # start_datetime_iso = start_datetime.isoformat()
+    # end_datetime_iso = end_datetime.isoformat()
+    print(time.time()-_start_time)
+    _start_time = time.time()
+    print("load CSV")
     df = pd.read_csv(settings.CSV_DATA_PATH)
-    df = df.fillna("undefined")
-    df['start_time'] = pd.to_datetime(df['start_time'])
-    df = df[df["start_time"].between(start_datetime_iso, end_datetime_iso)]
-    df = df.sort_values(by='start_time', ascending=True)
-    cols_vals = [filter.split(":") for filter in filters]
+    #df = df.fillna("undefined")
+    #df['start_time'] = pd.to_datetime(df['start_time'])
+    #df = df[df["start_time"].between(start_datetime_iso, end_datetime_iso)]
+    #df = df.sort_values(by='start_time', ascending=True)
+    # cols_vals = [filter.split(":") for filter in filters]
     
-    filters = {}
-    for pair in cols_vals:
-        if pair[0] in filters.keys():
-            filters[pair[0]].append(pair[1])
-        else:
-            filters[pair[0]] = [pair[1]]
+    # filters = {}
+    # for pair in cols_vals:
+    #     if pair[0] in filters.keys():
+    #         filters[pair[0]].append(pair[1])
+    #     else:
+    #         filters[pair[0]] = [pair[1]]
 
-    for key, value in filters.items():
-        df = df.loc[df[key].isin(value)]
+    # for key, value in filters.items():
+    #     df = df.loc[df[key].isin(value)]
     
+    print(time.time()-_start_time)
+    _start_time = time.time()
+    print("renaming fields")
     df['id'] = df.index
     # df['x'] = df['start_time']  # это делается на клиентской стороне с учетом локальной тайм-зоны
     df['y'] = df['wall_time']
+    print(time.time()-_start_time)
+    _start_time = time.time()
+    print("Crafting response")
     
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename=filtered_data.csv'
     df.to_csv(path_or_buf=response, index=False)
-    return response
+    print(time.time()-_start_time)
+    _start_time = time.time()
+    print("Returning response")
+    if cash == None:
+        cash = response
+    return cash
---
+++ static/js/custom/app.js
@@ -0,0 +1,226 @@
+var DiracChart = function() {
+    var app = this;
+    
+    this.StartApp =  function() {
+        this.Controller.initiateApp();
+    };
+
+    this.Configuration = {
+        data_source_url: "https://t1services.jinr.ru/rest/SSB/lastStatus?site_name=T1_RU_JINR",
+        startTime: moment(new Date(2020, 0, 1)),
+        datetime_format: 'YYYY-MM-DD HH:mm:ss',
+    };
+
+    this.Utils ={
+        getDuration: function(time) {
+            var now = new Date().getTime();
+            var error_time = new Date(time);
+            return moment.duration(now - error_time).humanize();
+        },
+    };
+
+    this.Model = {
+        data: null,
+        filters: null,
+        startTime: null,
+        endTime: null,
+        checkedFilters: null,
+        colorFilter: null,
+
+        initiateModel: function() {
+            this.startTime = app.Configuration.startTime.format(app.Configuration.datetime_format);
+            this.endTime = moment().format(app.Configuration.datetime_format);
+        },
+
+        getFilters: function() {
+            $.ajax({
+                url: "get_filters",
+                async: false,
+            }).done(function(response) {
+                app.Model.filters = response["filters"];
+                app.Model.currentFilters = this.filters;
+                console.log("Model: SUCCESS - filters successfully loaded");
+            }).fail(function(response) {
+                console.log("Model: ERROR - filter loading issue" + response);
+            })
+        },
+
+        setCheckedFilters: function(checkedFilters) {
+            this.checkedFilters = checkedFilters;
+        },
+
+        parseDataCSV: function() {
+            this.data = Papa.parse(this.dataCSV, {
+                header: true,
+                skipEmptyLines: true,
+                dynamicTyping: true,
+                fastMode: true,
+            }).data;
+            this.data.forEach(obj => {
+                // переводим в локальную и преобразовываем в миллисекунды (т.к. xAxis в master в датах умеет работать только с мс)
+                obj['start_time'] = new Date(obj['start_time']).getTime()
+                obj['x'] = obj['start_time']
+            })
+        },
+
+        getDataByFilters: function() {
+            this.colorFilter = $("#select_color_filter").val();
+            $.ajax({
+                xhr: function() {
+                    var xhr = new window.XMLHttpRequest();
+                    xhr.addEventListener("progress", function(evt) {
+                        if (evt.lengthComputable) {
+                            // var percentComplete = Math.round(evt.loaded / evt.total);
+                            $("#spinner p").text(`${Math.round(evt.loaded * 9.54 * Math.pow(10, -7))} MB`);
+                        }
+                    }, false);
+                    return xhr;
+                },
+                url: "get_data_by_filters",
+                data: {
+                    "filters[]": app.Model.checkedFilters,
+                    "start": app.Model.startTime,
+                    "end": app.Model.endTime
+                },
+                async: false,
+            }).done(function(response) {
+                $("#data_loading_preloader").css("display", "none");
+                $("#spinner p").text("loading");
+                app.Model.dataCSV = response;
+                app.Model.parseDataCSV();
+            });
+        },
+    };
+
+    this.View = {
+        showTooltip: function(el) {
+            $(".tooltip").remove();
+            var show_btn = $("<button />", {
+                text: 'Show',
+                class: 'btn btn-primary',
+                click: function () {
+                    if (FILTERS.length > 0) {
+                        $("#data_loading_preloader").css("display", "block");
+                        getDataByFilters(FILTERS, START_TIME, END_TIME);
+                    };
+                    $(".tooltip").remove();
+                }
+            });
+            var tooltip = new bootstrap.Tooltip(el, {
+                template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
+                placement: 'auto',
+                html: true,
+                title: show_btn,
+                trigger: "click manual",
+            });
+            tooltip.show();
+        },
+
+        createDateRangePicker: function() {
+            var start = moment(new Date(2020, 0, 1));
+            var end = moment();
+
+            function cb(start, end) {
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            }
+
+            $('#reportrange').daterangepicker({
+                timePicker: true,
+                startDate: start,
+                endDate: end,
+                maxYear: parseInt(moment().format('YYYY'), 10),
+                minYear: 2020,
+                minDate: start,
+                maxDate: moment(),
+                timePicker24Hour: true,
+                timePickerSeconds: true,
+                showDropdowns: true,
+                alwaysShowCalendars: true,
+                locale: {
+                    format: 'DD.MM.YYYY HH:MM:SS'
+                },
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                    'All Data': [moment(new Date(2020, 0, 1)), moment()]
+                }
+            }, cb);
+            cb(start, end);
+            return [start, end];
+        },
+
+        initiateView: function() {
+            var start_end = this.createDateRangePicker();
+            START_TIME = start_end[0].format(app.Configuration.datetime_format);
+            END_TIME = start_end[1].format(app.Configuration.datetime_format);
+            $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
+                START_TIME = picker.startDate.format(dt_format);
+                END_TIME = picker.endDate.format(dt_format);
+                this.showTooltip($(this));
+            });
+        },
+
+        drawFilter: function(selector, value, text) {
+            $(selector).append(`\
+                <div class="form-check">\
+                <input class="form-check-input" type="checkbox" checked value="${value}" id="${value}">\
+                <label class="form-check-label" for="${value}">\
+                    ${text}\
+                </label>\
+                </div>`
+            );
+        },
+
+        drawFilters: function(filters) {
+            $("#data_loading_preloader").css("display", "none");
+            for (category in filters) {
+                let id_name = "#" + category + "-selector p";
+                for (element in filters[category]) {
+                    var value = category + ":" + filters[category][element];
+                    var caption = filters[category][element];
+                    this.drawFilter(id_name, value, caption);
+                }
+            }
+        },
+
+        drawData: function(data) {
+            if (data.length > 0) {
+                DrawHighChart(data, app.Model.colorFilter);
+            } else {
+                $('#highcharts-container').html('<h4 class="text-primary">There is no data on this request</h4>\
+                <p>Change the request parameters and try again</p>');
+            }
+        },
+
+        getCheckedFilters: function() {
+            var checkedFilters=[];
+            $("input[type=checkbox]:checked").each(function() {
+                checkedFilters.push($(this).val())
+            });
+            console.log(checkedFilters);       
+            return checkedFilters;
+        }
+    };
+
+    this.Controller = {
+        initiateApp: function() {
+            app.Model.initiateModel();
+            app.View.initiateView();
+            app.Model.getFilters();
+            app.View.drawFilters(app.Model.filters);
+            this.getDataByFilters();           
+        },
+
+        getDataByFilters: function() {
+            let checkedFilters = app.View.getCheckedFilters();
+            app.Model.setCheckedFilters(checkedFilters);
+            app.Model.getDataByFilters();
+            app.View.drawData(app.Model.data);
+        },
+
+    }
+}
